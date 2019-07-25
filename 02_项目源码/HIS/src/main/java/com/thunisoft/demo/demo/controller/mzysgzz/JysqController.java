7d82fd22e68be4481ad600fb493dfb98f81f0ac6
余欣然 <yuxinran@thunisoft.com>
2019-07-25 18:39:40 +0800

    检验申请 成药处方（部分）

---
+++ 02_项目源码/HIS/src/main/java/com/thunisoft/demo/demo/controller/mzysgzz/JysqController.java
@@ -0,0 +1,27 @@
+package com.thunisoft.demo.demo.controller.mzysgzz;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * JysqController Controller
+ * 
+ *
+ * @author huayu
+ * @date 2019-07-25
+ */
+@Controller
+@RequestMapping("mzysgzz/jysq")
+public class JysqController {
+    /**
+     * html模板入口
+     * @return 'resources/templates'下的html模板
+     */
+    @GetMapping()
+    public String index(HttpServletRequest request) {
+        return "mzysgzz/jysq";
+ 	}
+}
