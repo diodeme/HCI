871d826482f4afcb17b4ec5f1e2f63be6dd81013
余欣然 <yuxinran@thunisoft.com>
2019-07-26 13:23:25 +0800

    门诊医生工作站

--- 02_项目源码/HIS/src/main/resources/static/js/mzysgzz/mzqz.js
+++ 02_项目源码/HIS/src/main/resources/static/js/mzysgzz/mzqz.js
@@ -1,16 +1,16 @@
 // **********************************************//
-// mzys 客户端脚本
+// mzqz 客户端脚本
 // 
 //
 // @author huayu
-// @date 2019-07-22
+// @date 2019-07-26
 // **********************************************//
 var vm = new Vue({
 	el: '#app',
 	mixins: [atyTplMixin],
 	data: function() {
-		
-		return {}
+		return {
+		}
 	},
 	methods: {
 	}
